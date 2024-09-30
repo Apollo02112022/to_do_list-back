0f86ead32e1ac1cd355d017621bcb68d20b726d9
Apollo02112022 <nicolas.fuente@sncf.fr>
2024-09-30 09:43:10 +0200

    backup-30/09/2024

---
+++ src/test/java/com/back/to_do_list_back/ToDoListBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.to_do_list_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
